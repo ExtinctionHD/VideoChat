4704f4b16d9c12b6b3485ba926a11425b66adc1c
ExtinctionHD <Extinction132@gmail.com>
2018-04-18 15:49:22 +0300

    Add first version with direct connection by IP address

---
+++ VoiceChat/Classes/BdtpClient.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using System.ComponentModel;
+
+namespace VoiceChat.Classes
+{
+    class BdtpClient: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return RemoteIP != null;
+            }
+        }
+
+        public IPAddress RemoteIP
+        {
+            get
+            {
+                return remoteIP;
+            }
+            private set
+            {
+                remoteIP = value;
+
+                if (value != null)
+                {
+                    tcpListener?.Stop();
+                    Thread thread = new Thread(new ThreadStart(WaitForDisconnect));
+                    thread.Start();
+                }
+
+                RaisePropertyChanged("IsConnected");
+            }
+        }
+        private IPAddress remoteIP;
+
+        private TcpListener tcpListener;
+        private TcpClient tcpController;
+
+        private UdpClient udpSender;
+        private UdpClient udpReceiver;
+
+        public int TcpPort { get; set; } = 11000;
+        public int SenderPort { get; set; } = 11001;
+        public int receiverPort { get; set; } = 11002;
+
+        public IPAddress LocalIP { get; set; }
+
+        public BdtpClient(IPAddress localIP)
+        {
+            LocalIP = localIP;
+
+            tcpListener = new TcpListener(LocalIP, TcpPort);
+                tcpController = new TcpClient();
+            udpSender = new UdpClient(new IPEndPoint(LocalIP, SenderPort));
+            udpReceiver = new UdpClient(new IPEndPoint(LocalIP, receiverPort));
+        }
+
+        public bool Connect(IPAddress remoteIP)
+        {
+            if (IsConnected)
+            {
+                return false;
+            }
+
+            try
+            {
+                tcpController = new TcpClient();
+                tcpController.Connect(remoteIP, TcpPort);
+            }
+            catch { return false; }
+
+            NetworkStream stream = tcpController.GetStream();
+            byte[] bytes = LocalIP.GetAddressBytes();
+            stream.Write(bytes, 0, bytes.Length);
+
+            RemoteIP = remoteIP;
+            return true;
+        }
+
+        public IPAddress Listen()
+        {
+            while (IsConnected) { }
+
+            tcpListener.Start();
+
+            try
+            {
+                tcpController = tcpListener.AcceptTcpClient();
+            }
+            catch { return null; }
+
+            RemoteIP = GetIPAddress(tcpController.GetStream());
+            return RemoteIP;
+        }
+
+        private IPAddress GetIPAddress(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+            int count = 0;
+
+            do
+            {
+                count += stream.Read(buffer, count, buffer.Length);
+            }
+            while (stream.DataAvailable);
+
+            byte[] bytes = new byte[count];
+            Array.Copy(buffer, bytes, count);
+
+            return new IPAddress(bytes);
+        }
+
+        public int Send(byte[] data)
+        {
+            if (!IsConnected)
+            {
+                return 0;
+            }
+
+            IPEndPoint remoteEP = new IPEndPoint(RemoteIP, receiverPort);
+            return udpSender.Send(data, data.Length, remoteEP);
+        }
+
+        public byte[] Receive()
+        {
+            if (!IsConnected)
+            {
+                return Array.Empty<byte>();
+            }
+
+            try
+            {
+                IPEndPoint senderEP = null;
+                return udpReceiver.Receive(ref senderEP);
+            }
+            catch
+            {
+                return Array.Empty<byte>();
+            }
+        }
+
+        public void Disconnect()
+        {
+            DisconnectBase();
+        }
+
+        public void Disconnect(Action callback)
+        {
+            DisconnectBase();
+            callback.Invoke();
+        }
+
+        private void DisconnectBase()
+        {
+            tcpListener.Stop();
+            tcpController.Close();
+
+            udpReceiver.Close();
+            udpReceiver = new UdpClient(new IPEndPoint(LocalIP, receiverPort));
+
+            RemoteIP = null;
+        }
+
+        private void WaitForDisconnect()
+        {
+            NetworkStream stream = tcpController.GetStream();
+
+            int count = 0;
+            do
+            {
+                byte[] buffer = new byte[256];
+                try
+                {
+                    count = stream.Read(buffer, 0, buffer.Length);
+                }
+                catch { }
+            }
+            while (count != 0 && IsConnected);
+
+            Disconnect();
+        }
+    }
+}
---
+++ VoiceChat/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using VoiceChat.Classes;
+
+namespace VoiceChat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IPAddress localIp;
+        private BdtpClient bdtpClient;
+
+        private WaveIn input;                       // Поток записи
+        private WaveOut output;                     // Поток воспроизведения
+        private BufferedWaveProvider bufferStream;  // Беферный поток для передачи через сеть
+
+        private Thread threadReceiver;  // Поток для прослушивания входящих сообщений
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            localIp = Dns.GetHostAddresses(Dns.GetHostName()).Where(x => x.AddressFamily == AddressFamily.InterNetwork).Last();
+            bdtpClient = new BdtpClient(localIp);
+            DataContext = bdtpClient;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtblockIP.Text = "Local IP: " + localIp.ToString();
+
+            InitializeAudio();
+
+            // Создаем поток для прослушки
+            threadReceiver = new Thread(new ThreadStart(Receiving));
+            threadReceiver.Start();
+
+            input.StartRecording();
+        }
+
+        private void InitializeAudio()
+        {
+            // Cоздаем поток для записи нашей речи определяем его формат - 
+            // частота дискретизации 8000 Гц, ширина сэмпла - 16 бит, 1 канал - моно
+            input = new WaveIn();
+            input.WaveFormat = new WaveFormat(8000, 16, 1);
+            input.DataAvailable += Voice_Input;
+
+            // Создание потока для прослушивания входящиего звука
+            output = new WaveOut();
+            bufferStream = new BufferedWaveProvider(new WaveFormat(8000, 16, 1));
+            output.Init(bufferStream);
+        }
+
+        // Обработка входящей речи
+        private void Voice_Input(object sender, WaveInEventArgs e)
+        {
+            bdtpClient.Send(e.Buffer);
+        }
+
+        private void Receiving()
+        {
+            while (true)
+            {
+                if (bdtpClient.Listen() != null)
+                {
+                    Dispatcher.Invoke(ShowRemoteIP);
+                }
+
+                output.Play();
+                while (bdtpClient.IsConnected)
+                {
+                    byte[] data = bdtpClient.Receive();
+                    bufferStream.AddSamples(data, 0, data.Length);
+
+                    Thread.Sleep(0);
+                }
+
+                output.Pause();
+                Thread.Sleep(0);
+            }
+        }
+
+        private void ShowRemoteIP()
+        {
+            txtboxIP.Text = bdtpClient.RemoteIP.ToString();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bdtpClient.Connect(IPAddress.Parse(txtboxIP.Text));
+            }
+            catch { }
+        }
+
+        private void btnReceive_Click(object sender, RoutedEventArgs e)
+        {
+            bdtpClient.Disconnect();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            bdtpClient.Disconnect();
+            threadReceiver.Abort();
+        }
+    }
+}
