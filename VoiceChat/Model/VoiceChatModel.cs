9dd2fc954cde4bb82445dd0b3a1f85c41bc21793
ExtinctionHD <Extinction132@gmail.com>
2018-05-27 20:16:56 +0300

    Remove and sort usings

--- VoiceChat/Model/VoiceChatModel.cs
+++ VoiceChat/Model/VoiceChatModel.cs
@@ -1,25 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net;
-using System.Threading;
-using System.Net.Sockets;
+﻿using BDTP;
 using NAudio.Wave;
-using NAudio.CoreAudioApi;
+using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Media;
 using System.Windows.Threading;
-using BDTP;
 
 namespace VoiceChat.Model
 {
